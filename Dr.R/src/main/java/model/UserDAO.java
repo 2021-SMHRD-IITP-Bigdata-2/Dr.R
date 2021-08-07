146b331668f4955eacebd8abc7d92e3d21ec4a11
smhrd <smhrd@183.105.79.189>
2021-08-07 09:47:49 +0900

    회원가입 / 로그인 / 회원수정 / 탈퇴를 위한 파일

---
+++ Dr.R/src/main/java/model/UserDAO.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+
+	private int cnt;
+
+	// 데이터베이스 연동기능, 종료기능
+	public void connection() {
+		try {
+			// 1.오라클 드라이버 동적 로딩 = 실행할때 찾아간다는 뜻
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2.데이터베이스 연동
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "hr";
+			String password = "hr";
+
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void close() {
+		// 4. 데이터베이스 연결종료
+		try {
+
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Dr.R/src/main/java/model/UserDTO.java
@@ -0,0 +1,56 @@
+package model;
+
+public class UserDTO {
+	private String u_id;
+	private String u_pw;
+	private String u_email;
+	private String u_name;
+	private int u_sex;
+	
+	public UserDTO(String u_id, String u_pw, String u_email, String u_name, int u_sex) {
+		super();
+		this.u_id = u_id;
+		this.u_pw = u_pw;
+		this.u_email = u_email;
+		this.u_name = u_name;
+		this.u_sex = u_sex;
+	}
+	
+	public String getU_id() {
+		return u_id;
+	}
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+	public String getU_pw() {
+		return u_pw;
+	}
+	public void setU_pw(String u_pw) {
+		this.u_pw = u_pw;
+	}
+	public String getU_email() {
+		return u_email;
+	}
+	public void setU_email(String u_email) {
+		this.u_email = u_email;
+	}
+	public String getU_name() {
+		return u_name;
+	}
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+	public int getU_sex() {
+		return u_sex;
+	}
+	public void setU_sex(int u_sex) {
+		this.u_sex = u_sex;
+	}
+	@Override
+	public String toString() {
+		return "UserDTO [u_id=" + u_id + ", u_pw=" + u_pw + ", u_email=" + u_email + ", u_name=" + u_name + ", u_sex="
+				+ u_sex + "]";
+	}
+	
+	
+}
