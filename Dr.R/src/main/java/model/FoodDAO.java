47c58e7cc0d1ea6ec9eed43635bf4b6d3f885a9d
smhrd <smhrd@183.105.48.223>
2021-08-09 16:54:25 +0900

    제철식재료 확인

--- Dr.R/src/main/java/model/FoodDAO.java
+++ Dr.R/src/main/java/model/FoodDAO.java
@@ -1,5 +1,89 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class FoodDAO {
+
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+
+	private int cnt;
+
+	// 데이터베이스 연동기능, 종료기능
+	public Connection connection() {
+		try {
+			conn = null;
+			// 1.오라클 드라이버 동적 로딩 = 실행할때 찾아간다는 뜻
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2.데이터베이스 연동
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String user = "cgi_3_3";
+			String password = "smhrd3";
+
+			conn = DriverManager.getConnection(url, user, password);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+
+	public void close() {
+		// 4. 데이터베이스 연결종료
+		try {
+
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ArrayList<FoodDTO> season_food(int month) {
+		
+		ArrayList<FoodDTO> list = new ArrayList<FoodDTO>();
+		
+		try {
+
+			connection();
+
+			// 3. 쿼리문 실행
+			String sql = "select food_code, food_name, food_image, food_good from Food where food_month=? order by food_name";
+
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, month);
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				int getFoodCode = rs.getInt(1);
+				String getName = rs.getString(2);
+				String getImage= rs.getString(3);
+				String getGood = rs.getString(4);
+				
+				FoodDTO food = new FoodDTO(getFoodCode, getName, getImage, getGood);
+				list.add(food);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally { // 무조건적으로 실행되는 finally
+			close();
+		} // end finally
+		return list;
+	}
 
 }
