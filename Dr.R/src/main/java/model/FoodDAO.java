6ef18007fbd7e1d22c79f89905602f8f07187696
smhrd <smhrd@112.187.117.248>
2021-08-09 21:18:13 +0900

    안함

--- Dr.R/src/main/java/model/FoodDAO.java
+++ Dr.R/src/main/java/model/FoodDAO.java
@@ -1,5 +1,59 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class FoodDAO {
 
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+
+	private int cnt;
+
+	// 데이터베이스 연동기능, 종료기능
+	public Connection connection() {
+		try {
+			conn = null;
+			// 1.오라클 드라이버 동적 로딩 = 실행할때 찾아간다는 뜻
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2.데이터베이스 연동
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String user = "cgi_3_3";
+			String password = "smhrd3";
+
+			conn = DriverManager.getConnection(url, user, password);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+
+	public void close() {
+		// 4. 데이터베이스 연결종료
+		try {
+
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 }
