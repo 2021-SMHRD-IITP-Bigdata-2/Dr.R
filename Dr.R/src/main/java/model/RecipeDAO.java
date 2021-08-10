c76cb0fad498586120759cc0e458ca7e26c1369d
smhrd <smhrd@183.105.48.223>
2021-08-10 15:05:54 +0900

    레시피 dao 살짝 만짐

--- Dr.R/src/main/java/model/RecipeDAO.java
+++ Dr.R/src/main/java/model/RecipeDAO.java
@@ -1,5 +1,92 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class RecipeDAO {
 
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+
+	private int cnt;
+
+	
+	public Connection connection() {
+		try {
+			conn = null;
+			// 1.오라클 드라이버 동적 로딩 = 실행할때 찾아간다는 뜻
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2.데이터베이스 연동
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String user = "cgi_3_3";
+			String password = "smhrd3";
+
+			conn = DriverManager.getConnection(url, user, password);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+
+	public void close() {
+		// 4. 데이터베이스 연결종료
+		try {
+
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 모든 레시피 저장 DAO
+	public ArrayList<RecipeDTO> all_recipe() {
+
+		ArrayList<RecipeDTO> list = new ArrayList<RecipeDTO>();
+
+		try {
+
+			connection();
+
+			// 3. 쿼리문 실행
+			String sql = "select * from Recipe order by racipe_name";
+
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				int getrecipe_code = rs.getInt(1);
+				String getrecipe_name = rs.getString(2);
+				String getrecipe_method = rs.getString(3);
+				String getrecipe_food = rs.getString(4);
+				String getrecipe_img = rs.getString(5);
+				String getrecipe_cook1 = rs.getString(6);
+				String getrecipe_cook2 = rs.getString(7);
+
+				RecipeDTO recipe = new RecipeDTO(getrecipe_code, getrecipe_name, getrecipe_method, getrecipe_food, getrecipe_img, getrecipe_cook1, getrecipe_cook2);
+				list.add(recipe);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally { // 무조건적으로 실행되는 finally
+			close();
+		} // end finally
+		return list;
+	}
+	
 }
